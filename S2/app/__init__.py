24346d969140bcd7c729899f09b4ff9e32d9a876
AB <bazigosa@gmail.com>
2023-11-02 13:19:07 +0000

    3rd Submission

--- S2/app/__init__.py
+++ S2/app/__init__.py
@@ -8,11 +8,14 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-# Set the cache control header to not store the imported static files in cache (they were giving me 304s)
+# For debugging
+"""
+# Set the cache control header to not store the imported static files in cache (I dislike 304s)
 @app.after_request
 def add_cache_control(response):
     response.headers['Cache-Control'] = 'no-store'
     return response
+"""
 
 from app import views, models
 
