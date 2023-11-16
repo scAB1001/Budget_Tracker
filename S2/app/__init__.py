4f26ce1f5436b2bf98d9b4073d77cc008624b288
AB <bazigosa@gmail.com>
2023-11-16 11:15:54 +0000

    Branching for cwk2

--- S2/app/__init__.py
+++ S2/app/__init__.py
@@ -9,13 +9,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # For debugging
-"""
 # Set the cache control header to not store the imported static files in cache (I dislike 304s)
 @app.after_request
 def add_cache_control(response):
     response.headers['Cache-Control'] = 'no-store'
     return response
-"""
 
 from app import views, models
 
